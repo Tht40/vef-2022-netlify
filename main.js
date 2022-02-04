ff0b8c70b99ca6ab0933f81e75ceae86f277bf47
Tht40 <77277019+Tht40@users.noreply.github.com>
2022-02-04 11:55:14 +0000

    add build script

---
+++ main.js
@@ -0,0 +1,27 @@
+import { writeFile } from 'fs/promises'
+
+async function main() {
+
+    const title = 'hallo';
+ const filename = 'index.html';
+ const data = `<!DOCTYPE html>
+ <html lang="is">
+ <head>
+   <meta charset="UTF-8">
+   <title>${title}</title>
+ </head>
+ <body>
+   <div>Halló heimur</div>
+ </body>
+ </html> 
+ `;
+    try{
+     const result = await writeFile(`./dist/${filename}`, data, { flag: 'w+'} );
+    }catch (e){
+        console.error(`Failed to create ${filename}`, e);
+    }
+}
+
+main().catch((e) => {
+    console.error('Error occured', e)
+});
